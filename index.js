e602c2e0b90ca67f8f48094d8d7e86e32d7b15f6
Svetlana Govyazina <govyazina@gmail.com>
2023-03-11 12:28:08 +0200

    some functions added

--- index.js
+++ index.js
@@ -1,17 +1,20 @@
 const filterByAge = (el) => {
-
+    return el.age < 50;
 };
 
 const filterByScore = (el) => {
+    return el.score > 75;
 
 };
 
 const filterByServiceRecord = (el) => {
-
+    return el.serviceRecord > 5;
 };
 
 const sortByScore = (arr) => {
-
+   const arrCopy = JSON.parse(JSON.stringify(arr))
+    arrCopy.sort((a, b) => b.score - a.score)
+    return arrCopy;
 };
 
 const getMeanAge = (arr) => {
